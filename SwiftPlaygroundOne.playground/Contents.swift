95fc14228afdfca95a5112c675942e64cd0a5f69
lilyanasky <lilyana@hawaii.edu>
2022-01-27 12:29:12 -1000

    Working on Week 2

---
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+var myFirstVariable: String
+var myFirstVariable = "I've created my first variable"
+
+// We're going to do something with strings
+var mySecondVariable = "and look I added two strings together"
+
+myFirstVariable + mySecondVariable
+
+var myThirdVariable = myFirstVariable + mySecondVariable
